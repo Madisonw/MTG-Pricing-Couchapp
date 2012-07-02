eb312045f819f752814fd5e34b958b12e8256be3
madisonw <madisonjw@gmail.com>
2012-07-01 17:45:32 -0700

    added rewrites and other skullduggery

--- _attachments/script/search.js
+++ _attachments/script/search.js
@@ -7,6 +7,10 @@
 		var oResult;
 		$(search_results).empty().show();
 		$(pricing_container).hide();
+		if (!results.length) {
+			$(search_results).append($("<div />",{text:"No results..."}));
+			return false;
+		}
 		$(results).each(function() {
 			oResult = $("<div />",{
 				text : this.key
@@ -23,24 +27,51 @@
 		})
 	}
 	function render_card_data(card) {
+		var LOADING = "retrieving...";
 		$(search_results).hide();
 		$(pricing_container).empty().show()
-		.append($("<h2 />",{text:card.name+" ("+card.manacost+")"}));
+		.append($("<h2 />",{text:card.name+" ("+card.manacost+")"}))
+		.append($("<span />",{text:"Touch the set bar for details","class":"details_text"}));
 		for (var set in card.sets) {
 			$(pricing_container).append(
 				$("<div />",{"class":"set","data-set":set_data[set].longname.toLowerCase()}).append(
 					$("<img />",{src:set_data[set].imgURL,alt:set}),
 					$("<span />",{text:set_data[set].longname}),
-					$("<span />",{text:"retrieving...","class":"pricing"})
-					
+					$("<span />",{text:LOADING,"class":"pricing"}),
+					$("<div />",{"class":"additional_data"}).css({display:"none"}).append(
+						$("<div />").append(
+							$("<span />",{text:"Low: "}),
+							$("<span />",{text:LOADING,"class":"low"})
+						),
+						$("<div />").append(
+							$("<span />",{text:"Avg: "}),
+							$("<span />",{text:LOADING,"class":"avg"})
+						),
+						$("<div />").append(
+							$("<span />",{text:"High: "}),
+							$("<span />",{text:LOADING,"class":"high"})
+						)
+					)
 				)
 			)
 		}
+		$(".set").click(function() {
+			$(this).find(".additional_data").toggle();
+			$(this).toggleClass("opened");
+			if ($(this).hasClass("opened")) {
+				$(this).animate({height:"80px"});
+			} else {
+				$(this).animate({height:"50px"});
+			}
+		})
 		$(searchbox).val("");
 		$.getJSON("http://blacklotusproject.com/json/?cards="+((card.name).replace(/\s/g,"+")),function(data){
 			$(data.cards).each(function() {
 				var set = this.url.split("/")[4].replace(/\+/g," ").replace("%3A",":").toLowerCase();
 				$("div[data-set='"+set+"'] span.pricing").html("$"+this.average).addClass("has_data");
+				$("div[data-set='"+set+"'] span.low").html("$"+this.low)
+				$("div[data-set='"+set+"'] span.avg").html("$"+this.average)
+				$("div[data-set='"+set+"'] span.high").html("$"+this.high)
 			})
 			$("span.pricing:not(.has_data)").html("no data");
 		})
@@ -58,7 +89,7 @@
 	$(searchbox).keyup(function() {
 		val = $(this).val();
 		if (!val.length) {
-			render_search_results([]);
+			$(search_results).hide();
 			return false;
 		}
 
